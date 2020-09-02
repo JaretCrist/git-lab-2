1593b382a3176a739654385c196f76987043f899
Jaret Crist <jc374719@ohio.edu>
2020-09-02 17:21:08 -0400

    Created c++ program file

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jaret Crist
+ *        Date: Sept. 2, 2020
+ * Description: Simple Cout program for Week 2's lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout<<"Hello Git!!"<<endl;
+
+    return 0;
+}// main
